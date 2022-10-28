6b9bfdb38225cb866d8ad8aef97035da98a265b2
Johan <sejongbgy07@gmail.com>
2022-10-29 01:10:33 +0900

    console

---
+++ 2-console/app.js
@@ -0,0 +1,24 @@
+console.log; // 개발
+console.info; // 정보
+console.warn; // 경보
+console.error; // 에러, 사용자 에러, 시스템 에러
+
+// assert
+console.assert; // 참이 아닐 때만 출력
+
+// print object
+console.table;
+console.dir;
+
+// 시간 측정
+
+console.time;
+console.timeEnd;
+
+// counting
+
+console.count;
+console.countReset;
+
+// 추적
+console.trace;
